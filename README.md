6a604f1f653111144dd2ff4afe8e889883c11252
saigesp <saigesp@gmail.com>
2021-08-09 21:59:57 +0200

    add indentation and sleep time args

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Translate vue-i18n with deepl API
 
+- Python 3 required
+
+Exmples:
+```
+python main.py /path/to/locales
+python main.py /path/to/file/en.json -l es
+python main.py /path/to/file/en_US.json --locale es --output es_ES.json --indent 2
+```
+> See more options with `python main.py --help`
+
 ## Install
 1. Create virtual environment
 
@@ -15,3 +25,4 @@ pip install -r requirements.txt
 DEEPL_AUTH_KEY=your-key-here
 ```
 > You can get a free deepl developer account in https://www.deepl.com/pro-checkout/account
+
--- main.py
+++ main.py
@@ -2,19 +2,15 @@
 import os
 import re
 import json
+import time
 import argparse
+from urllib import request, parse
 from dotenv import load_dotenv
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEEPL_API_ENDPOINT = 'https://api-free.deepl.com/v2/translate'
+SLEEP_BETWEEN_API_CALLS = 0.2 # Seconds
+INDENTATION_DEFAULT = 2
 
-"""
-Translate json files with deepl API
-- Python 3 required
-
-Usage:
-    python3 main.py -d /path/to/locales-folder/
-    python3 main.py -d /path/to/file.json -l en
-"""
 
 def find_files(path):
     """
@@ -43,24 +39,43 @@ def get_input_file(json_files):
         return os.path.join(input_dir, json_files[int(file_idx)])
 
 
-def get_target_lang_code(args):
+def get_output_file(output):
+    """
+    Get output file
+    """
+    output_file_name = output if output else f"{lang_code}.json"
+    if not output_file_name.endswith('.json'):
+        output_file_name += '.json'
+    output_file = os.path.join(os.path.dirname(input_file), output_file_name)
+    if os.path.exists(output_file):
+        override = input(f"File {output_file_name} already exists. Do you want to override it? [Y/N] ")
+        if not override.lower() in ('y', 'yes'):
+            output_file_name = input(f"Enter the new file name: ")
+            if not output_file_name.endswith('.json'):
+                output_file_name += '.json'
+            
+            return os.path.join(os.path.dirname(input_file), output_file_name)
+    return output_file
+
+
+def get_target_lang_code(locale):
     """
     Get language code from input
     """
-    lang_code = '' if not args.l else args.l
+    lang_code = '' if not locale else locale
     while len(lang_code) != 2:
         lang_code = input('Language code to translate to (2 letters): ')
 
-    return lang_code.upper()
+    return lang_code
 
 
-def get_strings_from_file(filepath, locale_target):
+def get_strings_from_file(filepath, target_locale, sleep_time):
     with open(filepath) as f:
         data = json.load(f)
-        return json_iterator(data, locale_target)
+        return json_iterator(data, target_locale, sleep_time)
 
 
-def json_iterator(data, locale_target, stored = {}):
+def json_iterator(data, target_locale, sleep_time, stored = {}):
     """
     Iterate over json file
     - These json files only contains dicts and strings, not arrays, booleans or numbers
@@ -68,33 +83,76 @@ def json_iterator(data, locale_target, stored = {}):
     for key, value in data.items():
         if type(value) == type(dict()):
             stored[key] = {}
-            stored[key] = json_iterator(value, locale_target, stored[key])
+            stored[key] = json_iterator(value, target_locale, sleep_time, stored[key])
         elif type(value) == type(str()):
-            stored[key] = translate_string(value, locale_target)
+            if value == '':
+                stored[key] = value
+            else:
+                time.sleep(sleep_time)
+                stored[key] = translate_string(value, target_locale)
     
     return stored
 
 
-def translate_string(string, locale_target):
-    if type(string) != type(str()):
-        return string
-    if string == '':
-        return string
+def translate_string(text, target_locale):
+    """
+    test with curl:
+    $ curl https://api-free.deepl.com/v2/translate -d auth_key=YOUR-API-KEY-HERE -d "text=Hello, world!" -d "target_lang=ES"
+    """
+    if type(text) != type(str()):
+        return text
 
-    return f"{string} in {locale_target}"
+    data = parse.urlencode({
+        'target_lang' : target_locale,  
+        'auth_key' : os.environ.get('DEEPL_AUTH_KEY'),
+        'text': text,
+    }).encode()
+    req = request.Request(DEEPL_API_ENDPOINT, data=data)
+    response = request.urlopen(req)
+
+    if response.status != 200:
+        print(f'{text}  ->  ERROR (response status {response.status})')
+        return text
+
+    response_data = json.loads(response.read())
+
+    if not 'translations' in response_data:
+        print(f'{text}  ->  ERROR (response empty {response_data})')
+        return text
+
+    print(text, ' -> ', response_data['translations'][0]['text'])
+
+    if len(response_data['translations']) > 1:
+        print(f"({text}) More than 1 translation: {response_data['translations']}")
+    
+    return response_data['translations'][0]['text']
+
+
+def save_results_file(data, output_file, indent):
+    """
+    Write output file
+    """
+    with open(output_file, 'w') as file:
+        json.dump(data, file, indent=indent)
+    
+    print(f"Results saved on {output_file}")
 
 
 if __name__ == "__main__":
-    load_dotenv(dotenv_path=os.path.join(CURRENT_DIR, '.env'))
-    if not os.environ.get('DEEPL_AUTH_KEY'):
+    load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env'))
+
+    if not os.environ.get('DEEPL_AUTH_KEY', False):
         raise Exception('Environment variables not loaded')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", help="Folder to look for translation files")
-    parser.add_argument("-l", help="Language target to translate")
+    parser.add_argument("file", help="Folder or file to look for translation source")
+    parser.add_argument("-o", "--output", help="Output file name")
+    parser.add_argument("-l", "--locale", help="Language target to translate")
+    parser.add_argument("-i", "--indent", type=int, default=INDENTATION_DEFAULT, help="Indentation spaces")
+    parser.add_argument("-s", "--sleep", type=float, default=SLEEP_BETWEEN_API_CALLS, help="Sleep time between API calls")
     args = parser.parse_args()
 
-    input_dir = os.path.normpath(args.d) if args.d else os.getcwd()
+    input_dir = os.path.normpath(args.file)
 
     if os.path.isdir(input_dir):
         json_files = find_files(input_dir)
@@ -108,12 +166,13 @@ if __name__ == "__main__":
             exit()
         input_file = os.path.normpath(input_dir)
 
-    lang_code = get_target_lang_code(args)
-    json_file_name = input_file.split('/')[-1].split('.')[0]
+    lang_code = get_target_lang_code(args.locale)
+    json_file_name = os.path.basename(input_file).split('.')[0]
 
     if lang_code.lower() == json_file_name.lower():
         print('You are trying to translate the same language!')
         exit()
 
-    result = get_strings_from_file(input_file, lang_code)
-    print('--> result', result)
+    output_file = get_output_file(args.output)
+    results = get_strings_from_file(input_file, lang_code.upper(), args.sleep)
+    save_results_file(results, output_file, args.indent)
